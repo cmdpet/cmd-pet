c817d24f34f6e43a18f098d45f9cdb948282d812
Marvin Chong <marvincht@gmail.com>
2020-03-03 18:47:15 +0800

    Implemented OOP
    The GameManager class holds most of the functions,
    and should be the main point of entry.
    
    It should be noted that some key terms and variables have been changed
    since some of them were effie.

---
+++ GameManager.py
@@ -0,0 +1,100 @@
+from Pet import Pet
+import webbrowser
+
+
+class GameManager:
+    version = '1.2.1'
+    command_list = ['actions', 'sleep', 'pet', 'feed', 'stats', 'feelings',
+                    'transfer', 'manual (for more commands)']
+
+    kinds = ['rock', 'fish', 'dog', 'cat']
+
+    def __init__(self):
+        self.pet = self.create_pet()
+        self.choose_actions()  # Put tutorial in final prod
+
+    def create_pet(self):
+        print(
+            f'CMD-PET <( o  o )> (v{self.version})\nby VukAnd and hellogoose.
+            \n'
+        )
+
+        print('welcome to the pet shop!')
+        self.display_kinds()
+        kind = input('which one would you like?')
+        while kind not in self.kinds:
+            print('we don\'t have that...')
+            self.display_kinds()
+            kind = input('please select one on the list.')
+        name = input('what would you like your new buddy to be named?\n')
+        player_pet = Pet(name, kind, 80, 100, 100)
+
+        print(f'this is {player_pet.name}, a {player_pet.kind}.')
+        print('<( o  o )>')
+        print('you need to take care of it.')
+        print('type actions to see all the actions!')
+
+        return player_pet
+
+    def tutorial(self):
+        self.actions_tutorial()
+
+    def actions_tutorial(self):
+        print('type actions to see all the actions!\n')
+        while True:
+            command = input('> ')
+            if command == 'actions':
+                self.display_actions()
+                self.choose_actions()
+            else:
+                print('a-c-t-i-o-n-s, type \'actions\'.')
+
+    def choose_actions(self):
+        while self.pet.is_alive:
+            command = input('> ')
+            if command == 'stats':
+                self.pet.display_stats()
+            elif command == 'sleep':
+                self.pet.sleep()
+            elif command == 'feed':
+                self.pet.fed()
+            elif command == 'actions':
+                self.display_actions()
+            elif command == 'pet':
+                self.pet.pet()
+            elif command == 'feelings':
+                self.pet.feelings()
+            elif command == 'transfer':
+                self.pet.transfer()
+            elif command == 'manual':
+                self.open_manual()
+            else:
+                print(f"{self.pet.name} doesn\'t understand that command.")
+
+        quit()
+
+    def open_manual(self):
+        confirmation = input(f'Open manual in your browser? (y/n)')
+        if confirmation == 'y':
+            print('Okay, opening now...')
+            webbrowser.open(
+                'https://github.com/cmdpet/cmd-pet/wiki', new=0, autoraise=True
+            )
+
+    def display_actions(self):
+        print('actions include:')
+        for command in self.command_list:
+            print(" -- ", command)
+
+    def display_kinds(self):
+        print('The pet types include:')
+        for kind in self.kinds:
+            print(" -- ", kind)
+
+    def shut_down(self):
+        print("closing application...")
+        self.pet.thread_event.clear()
+        self.pet.thread.join()
+
+
+gameManager = GameManager()
---
+++ Pet.py
@@ -0,0 +1,116 @@
+import time
+from random import randrange
+from collections import defaultdict
+from threading import Thread, Event
+
+
+class Pet:
+    raw_stats = {('energy', 'val'): 0,  # to be worked on
+                 ('energy', 'max'): 100,
+                 ('happiness', 'val'): 0,
+                 ('happiness', 'max'): 100,
+                 ('hunger', 'val'): 0,
+                 ('hunger', 'max'): 100}
+
+    stop_threads = False
+    is_alive = True
+
+    def __init__(self, name, kind, energy, happiness, hunger):
+        self.name = name
+        self.kind = kind
+
+        self.stats = defaultdict(dict)
+        for (attribute, detail), val in self.raw_stats.items():
+            self.stats[attribute][detail] = val
+
+        self.stats["energy"]["val"] = energy
+        self.stats["happiness"]["val"] = happiness
+        self.stats["hunger"]["val"] = hunger
+
+        self.thread_event = Event()
+        self.thread_event.set()
+
+        self.thread = Thread(
+            target=self.decrease_stats, args=(self.thread_event,)
+        )
+        self.thread.start()
+
+    def decrease_stats(self, thread_event):
+        frequency = 10
+        last_change = time.time()
+        while thread_event.is_set():
+            if (time.time() - last_change) > frequency:
+                last_change = time.time()
+                for attr in self.stats:
+                    self.add_to_stat(attr, (-1 * randrange(30)), False)
+
+    def display_stats(self):
+        for key, val in self.stats.items():
+            print(key + ": " + str(val['val']) + "/" + str(val['max']))
+
+    def sleep(self):
+        print(f'<(  u _ u )>\n{self.name} is sleeping...')
+        self.sleep_animation()
+        print(f'<( o  o )>\n{self.name} is awake!')
+        self.add_to_stat("energy", 30)
+
+    def sleep_animation(self):
+        time.sleep(randrange(60))
+
+    def fed(self):
+        print('    bread\n    ^  ^\n( o  o )')
+        time.sleep(0.5)
+        print('<( o <ead> o )>\nyummy!')
+        self.add_to_stat("hunger")  # makes the pet the full
+
+    def pet(self):
+        print('^( o  o )>')
+        time.sleep(0.5)
+        print('<( o  o )^')
+        self.add_to_stat("happiness", randrange(30))
+
+    def feelings(self):
+        threshold = self.stats['happiness']['max'] / 2
+        if self.stats['happiness']['val'] >= threshold:
+            print('<( ^ ^ )>\ni am very happy!')
+        elif self.stats['happiness']['val'] < threshold:
+            print('<( o  o )>\nfeeling okay!')
+
+    def transfer(self):
+        transferSure = input(f'\nAre you sure you want to transfer {self.name}?\nThis cannot be undone!\nEnter this to continue:\nI am sure I want to transfer my pet.\n')
+        if transferSure == 'I am sure I want to transfer my pet.':
+            transferTime = randrange(10)
+            print(f'\nTransferring {self.name}...\nThis should take about {transferTime} second(s).')
+            time.sleep(transferTime)
+            print(f'{self.name}, a {self.kind}, has been transferred.\nGoodbye, {self.name} :(')
+            time.sleep(2)
+            quit()
+        else:
+            print(f'Transfer of {self.name} has been cancelled.')
+
+    def add_to_stat(self, attr, value=100, display=True):
+        self.stats[attr]['val'] += value
+        if self.stats[attr]['val'] >= self.stats[attr]['max']:
+            self.stats[attr]['val'] = self.stats[attr]['max']
+        if display:
+            print(attr + " is now at " + str(self.stats[attr]['val']))
+
+    def check_status(self):
+        if self.stats['energy']['val'] < 50:
+            print('<(-  -)> i\'m tired')
+        if self.stats['hunger']['val'] < 50:
+            print('<(o  O  o)> i\'m hungry')
+        if self.stats['happiness']['val'] < 50:
+            print('<(T  T)> i\'m sad')
+        if self.stats['energy']['val'] < 0:
+            print(f'{self.name} has died due to being too tired. :(')
+            self.die()
+        elif self.stats['hunger']['val'] < 0:
+            print(f'{self.name} has died due to hunger. :(')
+            self.die()
+        elif self.stats['happiness']['val'] < 0:
+            print(f'{self.name} has died due to sadness. :(')
+            self.die()
+
+    def die(self):
+        self.is_alive = False
